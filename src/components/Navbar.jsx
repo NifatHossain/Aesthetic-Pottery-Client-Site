067649859753cc6cb8ac85a59d9e892384df8ee2
NifatHossain <nifathossain1480@gmail.com>
2024-04-30 22:59:53 +0600

    Responsive all working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     return (
         <div>
             <div className="navbar bg-base-100">
-                <div className="navbar-start">
+                <div className="navbar-start relative z-10">
                     <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -41,7 +41,7 @@ const Navbar = () => {
                         <li><Link to={`/myitems/${user?.email}`} className="font-semibold">My Art & Craft List</Link></li>
                     </ul>
                 </div>
-                <div className="navbar-end relative z-10">
+                <div className="navbar-end relative z-20">
                     {
                         user && <div>
                             <div id='avater' className="dropdown dropdown-end">
